4756920a91d692e787368203bf8d4f2562268989
Nomarg <94145617+Nomarg@users.noreply.github.com>
2022-12-24 20:12:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["an Artist", "a Developer", "a Designer", "a Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
